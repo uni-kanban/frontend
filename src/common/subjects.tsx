c8e9b0bb77147e07995e336bb6aae9776b92605f
v1rtl <hi@v1rtl.site>
2024-04-24 01:46:09 +0300

    send subject id

--- src/common/subjects.tsx
+++ src/common/subjects.tsx
@@ -2,15 +2,17 @@ import { h } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
 
 export const Subjects = () => {
-  const [subjects, setSubjects] = useState<{name:string}[]>([])
+  const [subjects, setSubjects] = useState<{ name: string; id: string }[]>([])
 
   useEffect(() => {
-    fetch('/api/get_subjects.php').then(r => r.json()).then(json => setSubjects(json))
+    fetch('/api/get_subjects.php').then((r) => r.json()).then((json) =>
+      setSubjects(json)
+    )
   }, [])
 
-  return <select>
-{
-  subjects.map(s => <option>{s.name}</option>)
-}
-  </select>
+  return (
+    <select name="subject">
+      {subjects.map((s) => <option value={s.id}>{s.name}</option>)}
+    </select>
+  )
 }
--- src/teacher/app.tsx
+++ src/teacher/app.tsx
@@ -2,3 +2,9 @@ import { h, render } from 'preact'
 import { Subjects } from 'src/common/subjects'
 
 render(<Subjects />, document.getElementById('subjects')!)
+
+const url = new URL(location.href)
+
+const subjectInput = (document.getElementById('subject_id') as HTMLInputElement)
+
+subjectInput.value = url.searchParams.get('subject') as string
